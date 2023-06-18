de17ef6071d3bc1deb522703c7be29b08b14d943
ByAlu <emrebelikirik4664@gmail.com>
2023-06-18 14:23:54 +0300

    -d

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from functions import *
 import numpy as np
 
 def main():
-    st.title("Finans Uygulaması")
+    st.title("Profit/Loss Graph Generator")
 
     min_strike_price = float('inf')
     max_strike_price = float('-inf')
@@ -12,19 +12,38 @@ def main():
 
     option_count = st.number_input("Kaç opsiyon kullanacaksınız:", min_value=1, value=1, step=1)
     stock_count = st.number_input("Kaç hisse senedi kullanacaksınız:", min_value=1, value=1, step=1)
-    stock_spot_price = st.number_input("Hisse senedi Spot Fiyatı:", value=0.0, step=1.0)
+    stock_spot_price = st.number_input("Hisse senedi Spot Fiyatı:", value=100.0, step=1.0)
 
-    st.write("Hisse senedi Bilgileri")
+    st.write("Hisse senedi Bilgileri", size="large")
     # hisse senedi alım mı satım mı bilgisi
-    stock_type = st.selectbox("Alım/Satım", ["Alım", "Satım"])
+    stocks = {}
+    for i in range(1, stock_count + 1):
+        stock_type = st.selectbox("Hisse Alım/Satım -" + str(i), ["Alım", "Satım"], key= 200+i)
+        stocks.update({i: {"type": stock_type}})
+
+    # find number of Alım and Satım in stocks
+    stock_buy_count = 0
+    stock_sell_count = 0
+    for stock in stocks.values():
+        if stock["type"] == "Alım":
+            stock_buy_count += 1
+        else:
+            stock_sell_count += 1
+    
+    if stock_buy_count >= stock_sell_count:
+        stock_type = "Alım"
+        stock_count = stock_buy_count-stock_sell_count
+    else:
+        stock_type = "Satım"
+        stock_count = stock_sell_count-stock_buy_count    
 
     options = {}
 
 
 
     for i in range(1, option_count + 1):
-        st.subheader(f"{i}. Opsiyon için bilgileri giriniz")
         st.write("-----------------------------")
+        st.subheader(f"{i}. Opsiyon için bilgileri giriniz")
         option_type = st.selectbox("Call/Put", ["C", "P"], key=4*i+1)
         option_buy_sell = st.selectbox("Alım-Satım", ["Alım", "Satım"], key=4*i+2)
         option_strike_price = st.number_input("Strike Fiyatı:", value=0.0, step=1.0, key=4*i+3)
@@ -51,9 +70,44 @@ def main():
 
 
     
-    stock_price_range = np.arange(min_strike_price-30, max_strike_price + 30, 1)
+    stock_price_range = np.arange(min_strike_price-20, max_strike_price + 20, 1)
 
-            
+    for key in options.keys():
+        option = options[key]
+        if option.get('type') == 'C':  
+            if option['strike_price'] < stock_spot_price:
+                if option['option_price'] < (stock_spot_price - option['strike_price']):
+                    st.write(str(key) + ". Call Opsiyonu için arbitraj fırsatı var")
+            elif option['strike_price'] > stock_spot_price:
+                if option['option_price'] >  (option['strike_price'] - stock_spot_price):
+                    st.write(str(key) + ". Call Opsiyonu için arbitraj fırsatı var")
+    
+        elif option.get('type') == 'P':
+            if option['strike_price'] < stock_spot_price:
+                if option['option_price'] > (stock_spot_price - option['strike_price']):
+                    st.write(str(key) + ". Put Opsiyonu için arbitraj fırsatı var")
+            elif option['strike_price'] > stock_spot_price:
+                if option['option_price'] < (option['strike_price'] - stock_spot_price):
+                    st.write(str(key) + ". Put Opsiyonu için arbitraj fırsatı var")
+
+
+    for key in options.keys():
+        #check different call options's strike price
+        if options[key]['type'] == 'C':
+            for key2 in options.keys():
+                if options[key2]['type'] == 'C':
+                    fst = abs(options[key]['strike_price']-stock_spot_price)
+                    snd = abs(options[key2]['strike_price']-stock_spot_price)
+                    if fst > snd and options[key]['option_price'] < options[key2]['option_price']:
+                        st.write(str(key) + ". ve " + str(key2) + ". Call Opsiyonları için arbitraj fırsatı var. Opsiyon-Fiyat dengesizliği.")
+        elif options[key]['type'] == 'P':
+            for key2 in options.keys():
+                if options[key2]['type'] == 'P':
+                    fst = abs(options[key]['strike_price']-stock_spot_price)
+                    snd = abs(options[key2]['strike_price']-stock_spot_price)
+                    if fst > snd and options[key]['option_price'] > options[key2]['option_price']:
+                        st.write(str(key) + ". ve " + str(key2) + ". Put Opsiyonları için arbitraj fırsatı var. Opsiyon-Fiyat dengesizliği.")
+
     if st.button("Grafik Çizdir"):
         all_payoffs = []
         st.write("Yeşil çizgi: Stock")
@@ -69,10 +123,11 @@ def main():
 
         plt.axhline(y=0, color='black', linestyle='-')
 
-        if stock_type == 'Alım':
-            plot_buy_stock_payoff(stock_spot_price, stock_price_range, stock_count, all_payoffs)
-        elif stock_type == 'Satım':
-            plot_sell_stock_payoff(stock_spot_price, stock_price_range, stock_count ,all_payoffs)
+        if stock_count != 0:
+            if stock_type == 'Alım':
+                plot_buy_stock_payoff(stock_spot_price, stock_price_range, stock_count, all_payoffs)
+            elif stock_type == 'Satım':
+                plot_sell_stock_payoff(stock_spot_price, stock_price_range, stock_count ,all_payoffs)
 
 
         for _ in options.keys():
@@ -97,11 +152,13 @@ def main():
         start_ys.append(sum)
         start_ys.sort()
 
-        plot_total_payoff(all_payoffs, stock_price_range, option_count)
+        plot_total_payoff(all_payoffs, stock_price_range, option_count, is_stock=stock_count != 0)
 
         y_bottom_limit = 0
 
         xs = [options[i+1]['strike_price'] for i in range(option_count)]
+        xs.append(stock_price_range[0])
+        xs.append(stock_price_range[-1])
 
         x0 = stock_price_range[0]
 
@@ -124,7 +181,7 @@ def main():
         plt.scatter(stock_spot_price, 0, marker='o', color='black')
 
         plt.yticks(start_ys, fontsize=7)
-        plt.xticks(xs , fontsize=7)
+        plt.xticks(xs , fontsize=6, rotation=90)
 
         
 
--- functions.py
+++ functions.py
@@ -4,24 +4,24 @@ import numpy as np
 min_strike_price = float('inf')
 max_strike_price = float('-inf')
 
-def get_buy_stock_payoff(strike_price, spot_price, many):
-    return (spot_price - strike_price) * many
+def get_buy_stock_payoff(stock_price, spot_price, many):
+    return (stock_price-spot_price) * many
 
-def get_sell_stock_payoff(strike_price, spot_price, many):
-    return (strike_price - spot_price) * many
+def get_sell_stock_payoff(stock_price, spot_price, many):
+    return (spot_price - stock_price) * many
 
-def plot_buy_stock_payoff(stock_spot_price, price_range, many , all_payoffs):
+def plot_buy_stock_payoff(spot_price, price_range, many , all_payoffs):
     payoff = []
     for price in price_range:
-        payoff.append(get_buy_stock_payoff(price, stock_spot_price, many))
+        payoff.append(get_buy_stock_payoff(price, spot_price, many))
 
     all_payoffs.append(payoff)
     plt.plot(price_range, payoff, color='green')
 
-def plot_sell_stock_payoff(stock_spot_price, price_range, many , all_payoffs):
+def plot_sell_stock_payoff(spot_price, price_range, many , all_payoffs):
     payoff = []
     for price in price_range:
-        payoff.append(get_sell_stock_payoff(price, stock_spot_price, many))
+        payoff.append(get_sell_stock_payoff(price, spot_price, many))
 
     all_payoffs.append(payoff)
     plt.plot(price_range, payoff, color='green')
@@ -76,12 +76,12 @@ def plot_put_payoff(strike_price, option_price, stock_price_range , all_payoffs)
     plt.plot(stock_price_range, payoffs, color='black', linewidth=0.7)  # Grafik rengini siyah olarak ayarla
 
     
-def plot_total_payoff(payoffs: list, stock_price_range, option_count):
+def plot_total_payoff(payoffs: list, stock_price_range, option_count, is_stock: bool):
     total_payoff = []
 
     for i in range(len(payoffs[0])):
         sum = 0
-        for _ in range(option_count+ 1):
+        for _ in range(option_count+ is_stock):
             sum+=payoffs[_][i]
         total_payoff.append(sum)
 
